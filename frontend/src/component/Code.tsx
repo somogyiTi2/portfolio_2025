614934d66b297807d46b892202042ddc0884b628
Somogyi Tibor <somogyi.tibor1997@gmail.com>
2025-04-22 21:23:22 +0200

    feature/HERO Profil code

---
+++ frontend/src/component/Code.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { CodeBlock } from "react-code-blocks";
+import { tomorrowNightEighties, paraisoLight } from "react-code-blocks";
+import { useTheme } from '../store/ThemeContext';
+import { useTranslation } from 'react-i18next';
+import '../i18n';
+
+const Code = () => {
+    const { theme } = useTheme();
+    const { t } = useTranslation();
+    const codeTheme = theme === 'dark' ? tomorrowNightEighties : paraisoLight;
+    const [codeText, setCodeText] = useState<string>("")
+    const [counter, setCounter] = useState(1);
+    const fullText = t('portfolioCode'); 
+
+    useEffect(() => {
+        if (counter <= fullText.length) {
+          const timeout = setTimeout(() => {
+            setCodeText(fullText.slice(0, counter));
+            setCounter((prev) => prev + 1);
+          }, 15);
+          return () => clearTimeout(timeout);
+        }
+      }, [counter]);
+
+    return (
+        <CodeBlock text={codeText} language="typescript" theme={codeTheme} />
+    )
+}
+
+export default Code
--- frontend/src/component/Hero.tsx
+++ frontend/src/component/Hero.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import '../i18n';
 import MeSVG from './MeSVG';
 import './Hero.css'
+import { useTheme } from '../store/ThemeContext';
+import Code from './Code';
 
 
 
 const Hero = () => {
-    const { t } = useTranslation();
-
+    const { theme } = useTheme();
     return (
         <div className='parent'>
             <div className='div1'>
-                <h1>{t('welcome')}</h1>
+                <div className={`rombus ${theme}`}>
+                <Code/>
+                </div>
             </div>
             <div className='div2'>
                 <MeSVG />
